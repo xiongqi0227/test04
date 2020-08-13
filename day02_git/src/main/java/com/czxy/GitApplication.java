785fd30aabc8b484dfaceb697b6d9764792532ce
熊祺 <xiongqi0227@163.com>
2020-08-13 10:40:37 +0800

    修改了pom文件，创建了启动类

---
+++ day02_git/src/main/java/com/czxy/GitApplication.java
@@ -0,0 +1,14 @@
+package com.czxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by xiongqi.
+ */
+@SpringBootApplication
+public class GitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GitApplication.class,args);
+    }
+}
